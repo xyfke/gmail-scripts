d3569d50aa78000b8ad37918f127f64e837c8f77
Fa Fa Ke <kexx7130@mylaurier.ca>
2025-02-21 13:39:03 -0500

    Count uniq employee file

---
+++ count_emp/count.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+
+    emp_file = open("inputs/results.txt", "r", encoding="utf-16")
+    hash_set = set()
+    result = open("outputs/uniq_emp.txt", "a")
+
+    for l in emp_file:
+        emp = l.strip().split("W2")[0]
+        hash_set.add(emp)
+
+    for emp in hash_set:
+        print(emp, file=result)
+
+    emp_file.close()
+    result.close()

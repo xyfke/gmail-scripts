0df08d49c2222f1497a3e9d4de6c2eeeb3d6ddc0
Fa Fa Ke <kexx7130@mylaurier.ca>
2025-02-25 09:28:25 -0500

    count unique employees in a file

--- count_emp/count.py
+++ count_emp/count.py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
 
-    emp_file = open("inputs/results.txt", "r", encoding="utf-16")
+    emp_file = open("inputs/aws.txt", "r", encoding="utf-16")
     hash_set = set()
     result = open("outputs/uniq_emp.txt", "a")
 
     for l in emp_file:
-        emp = l.strip().split("W2")[0]
+        emp = l.strip().split("W2")[0].strip()
         hash_set.add(emp)
 
     for emp in hash_set:
